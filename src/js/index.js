69ade8e2885bc0fa30dd55ee8089b76df8ef52e4
mr. whalen <michael.whalen@sitechhs.com>
2020-10-23 09:18:39 -0400

    sdfgsdfg

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-
+/* function getPkmn() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/mudkip`)
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+} */
+const pkmn = "mewtwo";
+async function getPkmn() {
+  try {
+    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`);
+    const data = await result.json();
+    console.log(data.name);
+  } catch (error) {
+    console.log(error);
+    alert("did you spell that correctly");
+  }
+}
+getPkmn();

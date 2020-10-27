4685c7cf0aa01d971ebfd91eb48b2e70f8b85638
mr. whalen <michael.whalen@sitechhs.com>
2020-10-27 09:56:39 -0400

    showing da movies

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,45 @@
-/* function getPkmn() {
-  fetch(`https://pokeapi.co/api/v2/pokemon/mudkip`)
-    .then((result) => {
-      return result.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-} */
-const pkmn = "mewtwo";
-async function getPkmn() {
+const DOMSelectors = {
+  grid: document.querySelector(".movie-grid"),
+};
+const key = "1fd276ec57b4baedacae00246e5cf4b7";
+const call = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=average_vote.asc&vote_count.gte=10000&vote_average.gte=8.1&include_adult=false&include_video=false&page=1`;
+
+const query = async function () {
   try {
-    const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmn}`);
-    const data = await result.json();
-    console.log(data.name);
+    const response = await fetch(call);
+    const data = await response.json();
+    data.results.forEach((movie) => {
+      DOMSelectors.grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="movie-card">
+      <div class="movie-card-front">
+        <img
+          src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+          alt=""
+          class="poster"
+        />
+      </div>
+      <div class="movie-card-back">
+        <h3 class="movie-card-header">${movie.original_title}</h3>
+        <div class="score-box">
+          <p class="user-score">Community Score</p>
+          <p class="user-score">${movie.vote_average}</p>
+        </div>
+
+        <div class="release-box">
+          <p class="release-date">Released</p>
+          <p class="release-date">${movie.release_date}</p>
+        </div>
+
+        <div class="movie-genres">
+       
+        </div>
+      </div>
+    </div>`
+      );
+    });
   } catch (error) {
     console.log(error);
-    alert("did you spell that correctly");
   }
-}
-getPkmn();
+};
+query();
